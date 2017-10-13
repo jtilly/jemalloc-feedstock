17608e4e4111a41b6e7ace5ba81aeea1e505b3a4
Uwe L. Korn <xhochy@users.noreply.github.com>
2017-10-13 20:51:39 +0200

    Update to jemalloc stable-4.x HEAD (#7)
    * Update to jemalloc stable-4.x HEAD
    
    * Add automake to build requirements

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+./autogen.sh
 ./configure --prefix=$PREFIX
+# Skip doc generation as the required docbook XSLs are not yet packaged on conda-forge
+touch doc/jemalloc.html
+touch doc/jemalloc.3
 make -j${CPU_COUNT}
 make install
