17ffc4c6a3e0787dda61e6bc8b8e7feb0744d073
Patrick Sodré <sodre@sodre.co>
2020-04-21 05:26:59 -0400

    Add run_exports to jemalloc 4.x (#17)
    * Add run_exports to jemalloc
    
    Update license to BSD-2-Clause
    
    Use name jemalloc
    
    Update CI/CD backend to azure
    
    Add compiler toolchain
    
    Update build.sh to match master branch
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.6.16, and conda-forge-pinning 2020.04.16.16.23.46
    
    * Update recipe/meta.yaml
    
    * Update build.sh
    
    * touch doc links
    
    * Update recipe/meta.yaml
    
    Please see review comments here:
    https://github.com/conda-forge/jemalloc-feedstock/pull/16/files#r409963877
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,20 @@
 set -e
 set -x
 
+# Static TLS has caused users to experience some errors of the form
+# "libjemalloc.so.2: cannot allocate memory in static TLS block"
+#
+# We disable this feature until we better understand how to avoid loader errors
+# of this type
 ./autogen.sh
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX \
+            --disable-tls \
+            --with-mangling=aligned_alloc:__aligned_alloc
+            
 # Skip doc generation as the required docbook XSLs are not yet packaged on conda-forge
-touch doc/jemalloc.html
+touch doc/jemalloc.html		             
 touch doc/jemalloc.3
+
+# Build
 make -j${CPU_COUNT}
 make install
